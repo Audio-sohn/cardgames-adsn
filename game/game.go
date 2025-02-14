68ced498e6006e9d5eca3357dd0a0f4d700269af
Audio-sohn <info@audiosohn.com>
2025-02-14 10:00:51 +0100

    update

--- game/game.go
+++ game/game.go
@@ -1,1 +1,9 @@
 package game
+
+// TODO
+// implement struct for the game
+
+// list of players
+// deck
+// heap of cards
+// winner
--- player/player.go
+++ player/player.go
@@ -1,1 +1,16 @@
 package player
+
+// define the Player interface
+// the interface is exposed
+
+type Player interface {
+
+	// make a move
+	GetMove()
+
+	// show the current Hand
+	GetHand()
+
+	// draw a new card
+	DrawCard()
+}
